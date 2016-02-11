e59fe1a4a6af0b2cb5553ab6b2987aec56a4bc97
whylu <a19870129@gmail.com>
2016-02-11 20:08:51 +0800

    ADD: repo/hdp2.3.4_centos6

--- repo/hdp2.3.4_centos6/download_untar.sh
+++ repo/hdp2.3.4_centos6/download_untar.sh
@@ -2,20 +2,31 @@
 
 mkdir ${APACHE_DOC_PATH}/hdp
 
+HDP_CHECKFILE=/opt/hdp_version
+HDP_UTILS_CHECKFILE=/opt/hdp-utils_version
 
-if [ ! -f ${HDP_OUTPUT} ]; then 
+
+if [ ! -f ${HDP_CHECKFILE} ]; then 
   echo "Now download hdp from "${HDP_URL}
   curl ${HDP_URL} -o ${HDP_OUTPUT}
   echo "Untar ${HDP_OUTPUT} to ${APACHE_DOC_PATH}/hdp"
+<<<<<<< HEAD:repo/hdp2.3.4_centos6/download_untar.sh
+  tar -zxf -C ${APACHE_DOC_PATH}/hdp ${HDP_OUTPUT}
+  echo ${HDP_VERSION} >> ${HDP_CHECKFILE}
+  rm -f ${HDP_OUTPUT}
+=======
   tar -zxf ${HDP_OUTPUT} -C ${APACHE_DOC_PATH}/hdp
+>>>>>>> b3b951214c0ef6e8300c6642f0214f6e753f0c2a:repo/2.2.0.0_centos6/download_untar.sh
 fi
 
 
-if [ ! -f ${HDP_UTILS_OUTPUT} ]; then 
+if [ ! -f ${HDP_UTILS_CHECKFILE} ]; then 
   echo "Now download hdp-utils from "${HDP_UTILS_URL}
   curl ${HDP_UTILS_URL} -o ${HDP_UTILS_OUTPUT}
   echo "Untar ${HDP_UTILS_OUTPUT} to ${APACHE_DOC_PATH}/hdp"
   tar -zxf ${HDP_UTILS_OUTPUT} -C ${APACHE_DOC_PATH}/hdp
+  echo ${HDP_UTILS_VERSION} >> ${HDP_UTILS_CHECKFILE}
+  rm -f ${HDP_UTILS_OUTPUT}
 fi
 
 
